d6b17539cbe2c5dba51c0f5f4fd43b8c8cdc621c
saishruthi <saishruthi.tn@ibm.com>
2020-06-04 08:11:51 -0700

    remove utils

--- aif360/aif360-r/tests/testthat/utils.R
+++
@@ -1,6 +0,0 @@
-# helper function to skip tests
-skip_if_no_py_modules <- function() {
-  have_scipy <- reticulate::py_module_available("aif360")
-  if (!have_scipy)
-    skip("AIF360 not available for testing")
-}
